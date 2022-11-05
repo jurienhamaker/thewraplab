743f235cb020bd4a2cebf632655d83ae41ac9559
Jurien Hamaker <jurien@employes.nl>
2022-11-05 16:59:30 +0100

    feat(Home): Section 80vh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import { Section } from "../components/Section";
 export default function Home() {
   return (
     <>
-      <Section id="home" className="h-screen"></Section>
-      <Section id="hoe" className="h-screen"></Section>
-      <Section id="wat" className="h-screen"></Section>
-      <Section id="waar" className="h-screen"></Section>
-      <Section id="contact" className="h-screen"></Section>
+      <Section id="home" className="h-[80vh]"></Section>
+      <Section id="hoe" className="h-[80vh]"></Section>
+      <Section id="wat" className="h-[80vh]"></Section>
+      <Section id="waar" className="h-[80vh]"></Section>
+      <Section id="contact" className="h-[80vh]"></Section>
     </>
   );
 }
