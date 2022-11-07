702fa7c565923a50a205b4d0816d08a22b54d65a
Jurien Hamaker <jurien@employes.nl>
2022-11-07 21:23:42 +0100

    feat: add slideInRight animation to car

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { HiOutlineChevronDown } from "react-icons/hi";
 
 import { Section } from "../components/Section";
-import { fadeGrowIn } from "../util/animations";
+import { fadeGrowIn, slideInRight } from "../util/animations";
 
 import Image from "next/image";
 
@@ -18,13 +18,14 @@ export const HomeSection: React.FunctionComponent<HomeSectionProps> = ({
   <Section id={id} className="relative flex h-[95vh] lg:h-[80vh]">
     <div className="h-80vh mx-auto flex items-center justify-center gap-4 px-4 md:max-w-[100vw] md:gap-14 lg:mt-0 lg:max-w-[90vw] lg:gap-0 xl:max-w-[80vw]">
       <div className="flex flex-col-reverse items-center justify-center lg:flex-row lg:pl-16 xl:pl-24">
-        <Image
-          src="/images/car.png"
-          width={500}
-          height={500}
-          alt="Yellow car"
-          className="mr-0 lg:-mr-16 xl:-mr-24"
-        />
+        <motion.div {...slideInRight} className="mr-0 lg:-mr-16 xl:-mr-24">
+          <Image
+            src="/images/car.png"
+            width={500}
+            height={500}
+            alt="Yellow car"
+          />
+        </motion.div>
 
         <div className="flex flex-col items-center text-center  lg:items-start lg:text-start">
           <motion.h1
