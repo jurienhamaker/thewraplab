c294c50a8941a627e291e5f3306902c8834fd977
Jurien Hamaker <jurien@employes.nl>
2023-01-02 14:02:23 +0100

    chore: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,28 @@ import "../styles/globals.scss";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { useEffect } from "react";
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
 import { getRandomColor } from "../util/colors";
+
+import * as snippet from "@segment/snippet";
+
+function renderSnippet() {
+  const opts = {
+    apiKey: process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY,
+    // note: the page option only covers SSR tracking.
+    // Page.js is used to track other events using `window.analytics.page()`
+    page: true,
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    return snippet.max(opts);
+  }
+
+  return snippet.min(opts);
+}
 
 const TheWrapLab = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
@@ -23,6 +41,10 @@ const TheWrapLab = ({ Component, pageProps }: AppProps) => {
         <title>The wrap lab</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <Script
+        id="segment-script"
+        dangerouslySetInnerHTML={{ __html: renderSnippet() }}
+      />
 
       <Navigation />
       <Component {...pageProps} />
